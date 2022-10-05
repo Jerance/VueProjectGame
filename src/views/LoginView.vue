05dbcf5ace4b3aa709e9799aba6d7c116f40dc20
Jerance <michael.jerance@gmail.com>
2022-10-05 15:49:49 +0200

    SignUp / Login request post api functional

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,56 @@
 <template>
-  <h1>LoginViewPage</h1>
+  <h1>LoginPage</h1>
+  <div class="form-container">
+    <form @submit.prevent="sendPostRequest()" class="login-form">
+      <div class="username-form">
+        <label for="username">UserName</label
+        ><input type="text" name="username" id="username" v-model="username" />
+      </div>
+      <div class="password-form">
+        <label class="username-label" for="password">Password</label
+        ><input type="text" name="password" id="password" v-model="password" />
+      </div>
+      <button type="submit">Confirm</button>
+    </form>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    sendPostRequest() {
+      axios
+        .post("http://apigame.co/auth/login", {
+          username: this.username,
+          password: this.password,
+        })
+        //
+        .then((response) => {
+          console.log(response);
+          console.log(response.data);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+form.login-form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,3 +1,55 @@
 <template>
   <h1>SignUpPage</h1>
+  <div class="form-container">
+    <form @submit.prevent="createPostRequest()" class="sign-up-form">
+      <div class="username-form">
+        <label for="username">UserName</label
+        ><input type="text" name="username" id="username" v-model="username" />
+      </div>
+      <div class="password-form">
+        <label class="username-label" for="password">Password</label
+        ><input type="text" name="password" id="password" v-model="password" />
+      </div>
+      <button type="submit">Confirm</button>
+    </form>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    createPostRequest() {
+      axios
+        .post("http://apigame.co/auth/signup", {
+          username: this.username,
+          password: this.password,
+        })
+        .then((response) => {
+          console.log(response);
+          console.log("test");
+        });
+    },
+  },
+};
+</script>
+
+<style>
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+form.sign-up-form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
