a3abff411eeafc745805dd19af77bb874a32875b
Jerance <michael.jerance@gmail.com>
2022-10-06 14:05:18 +0200

    Creation routes / views Market and fix redirection login

---
+++ src/views/MarketView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Market</h1>
+  <router-link to="/market">Communuty Offers</router-link>
+  <router-link to="/market/mytrades">My Trades</router-link>
+  <router-link to="/market/newtrades">New Trade +</router-link>
+</template>
+
+<script>
+import { mapActions, mapState } from "pinia";
+import { useGameStore } from "../store/myStore.js";
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  computed: {
+    ...mapState(useGameStore, ["authToken"]),
+  },
+  methods: {
+    ...mapActions(useGameStore, ["login"]),
+  },
+};
+</script>
+
+<style></style>
