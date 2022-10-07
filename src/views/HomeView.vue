3681aa4a704d86cc388a9ba4cb76581cecdc5290
Julien ROBIN <julien.jean.robin@gmail.com>
2022-10-07 11:50:44 +0200

    update router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div v-if="authToken" class="home">
+    <router-link to="/new-factory">New Factory</router-link>
+    <HelloWorld msg="A retirer" />
   </div>
+  <h1 v-else>Please login or signup</h1>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { mapState } from "pinia";
+import { useGameStore } from "../store/myStore";
 
 export default {
   name: "HomeView",
   components: {
     HelloWorld,
+  },
+  computed: {
+    ...mapState(useGameStore, ["authToken"]),
   },
 };
 </script>
